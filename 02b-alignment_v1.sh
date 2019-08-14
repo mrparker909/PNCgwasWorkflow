58ca9e645f0187f961ad832264da0aeaa0cbf61d
Matthew Parker <mrparker909@gmail.com>
2019-08-14 13:49:09 -0700

    Create 02b-alignment_v1.sh

---
+++ 02b-alignment_v1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#PBS -q medium
+
+#PBS -N preImputeAlign-v1
+#PBS -l mem=30gb
+#PBS -l walltime=1:00:00
+#PBS -l procs=1
+
+#PBS -o /zfs3/users/matthew.parker/matthew.parker/PBSlogs/log_$PBS_JOBID.txt
+#PBS -e /zfs3/users/matthew.parker/matthew.parker/PBSlogs/err_$PBS_JOBID.txt
+
+echo "Changing to directory containing gwasqc."
+cd /zfs3/users/matthew.parker/matthew.parker/PBSscripts/GenotypeImputation/gwasqc/out
+echo "Starting script at `date`"
+
+module load plink2
+
+HRCDir="../../HRC-1000G-check-bim-v4.2.11-NoReadKey/HRC-1000G-check-bim-NoReadKey.pl"
+REFDir="../../HRC.r1-1.GRCh37.wgs.mac5.sites.tab"
+
+perl $HRCDir -b GO_v1_hg18.bim -f GO_v1_hg18.post.frq -r $REFDir -h
+# DO NOT FORGET TO RUN THIS AFTER THIS SCRIPT COMPLETES, BEFORE CONTINUING FURTHER: sh Run-plink.sh
+
+echo "script completed at `date`"

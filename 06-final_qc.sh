d0808673a456bd295d481e27acb2d3b00df63025
Matthew Parker <mrparker909@gmail.com>
2019-08-23 14:02:15 -0700

    Update 06-final_qc.sh

--- 06-final_qc.sh
+++ 06-final_qc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -q medium
 
-#PBS -N Final QC
+#PBS -N Final_QC
 #PBS -l mem=30gb
 #PBS -l walltime=100:00:00
 #PBS -l procs=1
@@ -14,19 +14,21 @@ module load plink2
 
 cd /zfs3/scratch/saram_lab/PNC/data/genotypeImputed/hrc/merged/
 
-inFile="hrc.merged"
+inFile="hrc_merged"
 outFile="hrc.cleaned"
 assayFile="/zfs3/scratch/saram_lab/PNC/data/genotype/qc/EAsubjectOnly/post-align/assayed.txt"
+
+plink --vcf ${inFile}.vcf.gz --make-bed --out tmp
 
 # scan for incorrect strand assignment
 # see plink.flipscan for main report
 echo "Beginning flip-scan..."
-plink --bfile $inFile --flip-scan
+plink --bfile tmp --flip-scan
 
 echo "Beginning LD pruning..."
 # LD-based pruning
-plink --bfile $inFile --extract $assayFile --indep-pairwise 1500 150 0.2
-plink --bfile $inFile --extract pruned --genome 0.05 gz
+plink --bfile tmp --extract $assayFile --indep-pairwise 1500 150 0.2
+plink --bfile tmp --extract pruned --genome
 
 echo "Beginning IBD cuts..."
 # Cryptic relatedness check

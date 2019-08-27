535f15de4f24d876bbe04fcf162dd982b55606b9
Matthew Parker <mrparker909@gmail.com>
2019-08-27 09:48:52 -0700

    Create 07b-pca.sh

---
+++ 07b-pca.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PBS -q medium
+
+#PBS -N PCA
+#PBS -l mem=10gb
+#PBS -l walltime=100:00:00
+#PBS -l procs=1
+
+#PBS -o /zfs3/users/matthew.parker/matthew.parker/PBSlogs/log_$PBS_JOBID.txt
+#PBS -e /zfs3/users/matthew.parker/matthew.parker/PBSlogs/err_$PBS_JOBID.txt
+
+module load eigensoft
+
+cd /zfs3/scratch/saram_lab/PNC/data/genotypeImputed/hrc/postQC/
+
+smartpca -p eigensoft_params.txt

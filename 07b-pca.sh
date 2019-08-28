c4d659177ba9e1c37285391ff4383dc935b0c184
Matthew Parker <mrparker909@gmail.com>
2019-08-28 11:48:06 -0700

    Update 07b-pca.sh

--- 07b-pca.sh
+++ 07b-pca.sh
@@ -3,7 +3,7 @@
 
 #PBS -N PCA
 #PBS -l mem=10gb
-#PBS -l walltime=100:00:00
+#PBS -l walltime=10:00:00
 #PBS -l procs=1
 
 #PBS -o /zfs3/users/matthew.parker/matthew.parker/PBSlogs/log_$PBS_JOBID.txt
